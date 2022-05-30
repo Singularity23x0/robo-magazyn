61ecab05182fd0c80eb3592a3129ad9b6ffbb39f
Kacper Kafara <kacperkafara@gmail.com>
2022-05-30 08:12:02 +0200

    chore: use data from config for calculations

--- src/main.cpp
+++ src/main.cpp
@@ -44,67 +44,39 @@ int main(int argc, char const *argv[])
 
     string result = readInput(datapath + "/base-case.json");
 
-    LOG(INFO) << result;
-
     json configurationJson = json::parse(result);
-    LOG(INFO) << configurationJson;
-
     Configuration config = configurationJson.get<Configuration>();
 
-    LOG(INFO) << "Iterations: " << config.iterations;
-    LOG(INFO) << "Robot count: " << config.robotCount;
-    LOG(INFO) << "Initial positions: ";
-    for (auto &position : config.robotPositions) {
-        cout << position.row << " " << position.col << '\n';
-    }
-    cout << '\n';
+    // LOG(INFO) << "Iterations: " << config.iterations;
+    // LOG(INFO) << "Robot count: " << config.robotCount;
+    // LOG(INFO) << "Initial positions: ";
+    // for (auto &position : config.robotPositions) {
+    //     cout << position.row << " " << position.col << '\n';
+    // }
+    // cout << '\n';
 
-    LOG(INFO) << "Orders:";
-    for (auto &order : config.orders) {
-        for (auto &p : order) {
-            cout << p << " ";
-        }
-        cout << '\n';
-    }
+    // LOG(INFO) << "Orders:";
+    // for (auto &order : config.orders) {
+    //     for (auto &p : order) {
+    //         cout << p << " ";
+    //     }
+    //     cout << '\n';
+    // }
 
 
     setRobotsAmount(config.robotCount);
     setMagazineSize(config.magazineHeight, config.magazineWidth);
 
-    // vector<vector<int>> magazine = config.magazine;
-
-    vector<vector<int>> magazine = {
-        {-1, 1, 0, 1, -1},
-        {2, 2, 0, 2, 2},
-        {3, 3, 0, 3, 3},
-        {4, 4, 0, 4, 4},
-        {5, 5, 0, 5, 5},
-        {-1, 2, 3, 4, -1}
-    };
+    vector<vector<int>> magazine = config.magazine;
+    vector<Position> robotPositions = config.robotPositions;
+    vector<Position> robotEndPositions = config.robotEndPositions;
+    vector<set<int>> orders = config.orders;
     
-    Position robotPositions[4] = {
-        Position{0, 0},
-        Position{4, 4},
-        Position{2, 4},
-        Position{4, 1}};
-
-    Position robotEndPositions[4] = {
-        Position{0, 0},
-        Position{5, 0},
-        Position{0, 4},
-        Position{5, 4}};
-
-    set<int> orders[4] = {
-        set<int>{0, 1, 3},
-        set<int>{0, 1, 5},
-        set<int>{1, 3, 4},
-        set<int>{2, 4}};
-
     vector<Solution> population;
 
     for (int i = 0; i < 20; i++) {
         try {
-            vector <vector <Move>> moves = simulate(magazine, robotPositions, robotEndPositions, orders);
+            vector <vector <Move>> moves = simulate(magazine, robotPositions.data(), robotEndPositions.data(), orders.data());
             population.push_back(Solution{moves});
         } catch (...) {
             i--;
@@ -137,7 +109,7 @@ int main(int argc, char const *argv[])
     LOG(INFO) << "Converting solution to JSON format";
     json j = solution->moves;
     LOG(INFO) << "Writing solution in JSON format to stdout";
-    std::cout << std::setw(4) << j << std::endl;
+    // std::cout << std::setw(4) << j << std::endl;
 
     // for(std::size_t i = 0; i < solution.size(); i++)
     // {
